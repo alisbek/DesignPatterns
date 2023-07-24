d82b03449824287801678cd28cd00adae3423a79
AlisAlymkulov <61144938+AlisAlymkulov@users.noreply.github.com>
2023-07-24 13:33:13 +0200

    Concept for adapter pattern

---
+++ DesignPatterns/Adapter/Adapter.cs
@@ -0,0 +1,15 @@
+﻿class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+
+    public string GetRequest()
+    {
+        return $"This is '{_adaptee.GetSpecificRequest()}'";
+    }
+}
